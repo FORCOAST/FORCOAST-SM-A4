821ad0192d01d20e8d47ce8947da31555b1c14c0
danieltwigt <50665134+danieltwigt@users.noreply.github.com>
2022-06-08 22:40:46 +0200

    Copy bulletin to initial dir to avoid run failing
    Copy bulletin to initial dir to avoid run failing

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# docker run forcoast-sm-a3 ...
+# docker run forcoast-sm-a4 ...
 INITIAL_DIR="$(pwd)"
 cd /usr/src/app
 python ./temp_oystergrounds_erddap_LB_WIP.py $1 $2 $3 $4 $5 $6 $7
 python ./Bulletin/send_bulletin.py -A $1 -B $2 -C $3 -D $4 -E $5 -F $6 -G $7 -H $8 -I $9 -J ${10} -K ${11}
+
+cp ${10} $INITIAL_DIR
